22767e9daa759afb261895d5cad9afeea382ed1c
Miguel Lima <miguellimaoliver97@gmail.com>
2021-09-17 15:19:03 -0300

    Terceiro Commit

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
 
+
+/* Step by step to draw on the canvas:
+- When the mouse click Download, enable Drawing mode.
+- When the mouse moves, if the drawing mode is enabled, draw.
+- When the mouse click rise, disable the Drawing mode.
+*/
+
 // FUNCTIONS 
 function colorClickEvent(e) {
     let color = e.target.getAttribute('data-color');
